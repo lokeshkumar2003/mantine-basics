fd683e29367457eea9ce7317123273b8b581051c
lokeshkumar2003 <lokeshravi2003@gmail.com>
2022-11-28 16:51:59 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button, Loader, MantineProvider, Paper, Text } from "@mantine/core";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MantineProvider
+        theme={{
+          fontFamily: "Open Sans",
+          colorScheme: "dark",
+          fontSizes: { md: 12 },
+          loader:"oval",
+          radius: { sm: 232 },
+          colors: {
+            blue: [
+              "#7950F2",
+              "#66D9E8",
+              "#12B886",
+              "#51CF66",
+              "#66A80F",
+              "#FFE066",
+              "#FF922B",
+              "#DBE4FF",
+              "#2B8A3E",
+              "#D9480F",
+            ]
+          },
+        }}
+        styles={{
+          Button:(theme)=>({
+            root:{
+              backgroundColor: theme.colors.blue[1]
+            }
+          })
+        }}
+      >
+        <Paper>
+          <Text>this is a simple text tag</Text>
+          <Text>this is a simple text tag</Text>
+        </Paper>
+        <Button>Hello</Button>
+        <Button>Hello</Button>
+        <Loader />
+      </MantineProvider>
     </div>
   );
 }
