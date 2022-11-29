264cb591b6b06a89e65c4ebcbe150338240bccf3
lokeshkumar2003 <lokeshravi2003@gmail.com>
2022-11-29 20:49:16 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,11 @@
 import "./App.css";
-import { Button, Loader, MantineProvider, Paper, Text } from "@mantine/core";
+import Card from "./Components/Cards";
 
 
 function App() {
   return (
     <div className="App">
-      <MantineProvider
-        theme={{
-          fontFamily: "Open Sans",
-          colorScheme: "dark",
-          fontSizes: { md: 12 },
-          loader:"oval",
-          radius: { sm: 232 },
-          colors: {
-            blue: [
-              "#7950F2",
-              "#66D9E8",
-              "#12B886",
-              "#51CF66",
-              "#66A80F",
-              "#FFE066",
-              "#FF922B",
-              "#DBE4FF",
-              "#2B8A3E",
-              "#D9480F",
-            ]
-          },
-        }}
-        styles={{
-          Button:(theme)=>({
-            root:{
-              backgroundColor: theme.colors.blue[1]
-            }
-          })
-        }}
-      >
-        <Paper>
-          <Text>this is a simple text tag</Text>
-          <Text>this is a simple text tag</Text>
-        </Paper>
-        <Button>Hello</Button>
-        <Button>Hello</Button>
-        <Loader />
-      </MantineProvider>
+      <Card />
     </div>
   );
 }
---
+++ src/Components/LightDarkButton.tsx
@@ -0,0 +1,11 @@
+
+
+function LightDarkButton() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default LightDarkButton;
